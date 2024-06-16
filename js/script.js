d36399f3fb324f1c81e489aa01fd7d4c46d21169
ssamarin <semenssamarin@mail.ru>
2024-06-16 18:49:45 +0300

    va

---
+++ js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu__list'),
+    menuItem = document.querySelectorAll('.menu__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu__list_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu__list_active');
+        })
+    })
+})
